bf6de42c91661099a51c1639f8fa4e305eddfae2
cippero <gilwein@gmail.com>
2018-04-14 19:16:55 -0700

    got routes working, will format info on ejs pages next

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 /* dependencies & app setup */
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const app = express();
+const express    = require('express');
+const path       = require('path');
+const logger     = require('morgan');
+const app        = express();
+const pizza      = require("./db/pizza");
+const pizzaRoute = require("./routes/pizza");
 
 /* setting up port & listen */
 const PORT = process.env.PORT || 3000;
@@ -17,13 +19,41 @@ app.set('view engine', 'ejs');
 /* error logger, static routes */
 app.use(logger('dev'));
 app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/pizza', pizzaRoute);
 
 
-app.get("/sample", function(req,res){
-	// here's a sample route
+//test
+
+app.get("/", function(req,res){
+	res.render("index", {pizza: pizza});
 })
+
+// app.get("/pizza", function(req,res){
+// 	res.render("pizza/pizza-index", {pizza: pizza});
+// })
+
+// app.get("/pizza/:id", function(req,res){
+// 	let id = Number(req.params.id) + 1;
+// 	res.render("pizza/pizza-single", {pizza: pizza, id: id});
+// })
+
+//test
+
+
 
 /* error handler */
 app.get('*', function(req, res) {
   res.status(404).send({message: 'Oops! Not found.'});
 });
+
+// pizza-index.ejs
+// <!-- When rendered in the browser, the /pizza view should:
+//   - have a list of every pizza flavor
+//   - the pizza flavors should also have the location and the picture
+// --> 
+
+// pizza-single.ejs
+// <!--
+//   This view should only display one pizza, with flavor, location, and picture.
+// -->
+
--- routes/pizza.js
+++ routes/pizza.js
@@ -1,6 +1,8 @@
 /* set up the router */
-
+const express    = require('express');
+const pizzaRoute = express.Router();
 /* import the pizza data */
+const pizza = require("../db/pizza");
 
 /* you'll need two routes:
   - all pizza
@@ -8,4 +10,14 @@
   - BONUS: can you get individual pizza to work with the `flavorKey` property in the database? Maybe use some array methods... it would be convenient to be able to go to `/pizza/pepperoni` instead of `/pizza/0`.
 */
 
+pizzaRoute.get("/", function(req,res){
+	res.render("pizza/pizza-index", {pizza: pizza});
+})
+
+pizzaRoute.get("/:id", function(req,res){
+	let id = Number(req.params.id) + 1;
+	res.render("pizza/pizza-single", {pizza: pizza, id: id});
+})
+
 /* export the router */
+module.exports = pizzaRoute;
